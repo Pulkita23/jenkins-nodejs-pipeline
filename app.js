8d34df3d5b226692e7ae12310b0803196c4fc373
Pulkita23 <160568994+Pulkita23@users.noreply.github.com>
2025-04-11 10:19:10 +0530

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Hello Pulkita!'));
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(4000, () => console.log('Server running on port 4000'));
 
